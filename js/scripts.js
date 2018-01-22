0acf44477a1eea4cbba73f0bf55bdce02a9f7afe
Alexander Neumann and Victor Felix <alexander@>
2018-01-22 10:21:04 -0800

    Add checkFunction. Add returnName. Add displayFunction. Add form input acquisition. Add comparatorFunction.

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,57 @@ address.prototype.cEmail = function(input){
 address.prototype.returnAllInformation=function(){
   return "Name: " + this.first + ", " + this.last + "\n" + "Birthday: " + this.birth + "\n" + "Address: " + this.address + "\n" + "Phone: " + this.phone + "\n" + "Email: " + this.email;
 }
+
+function returnName(inputName, selector){
+  var output = inputName.split(" ")
+  if(selector === 0){
+    return output[0];
+  } else if(selector===1) {
+    return output[1];
+  }
+  return -1;
+}
+function toggleThePage(){
+  $("form#addressForm").toggle();
+  $("results").toggle();
+}
+
+function checkFunction(input){
+  var arrLength = myAddresses.length;
+  for(var i = 0; i < arrLength; i ++){
+    var currentValue = myAddresses[i];
+    if(input.first === this.first &&input.last === this.last &&input.birth === this.birth &&input.address === this.address &&input.phone === this.phone &&input.email === this.email){
+      return "Sorry duplicate information.";
+    } else if(input.email === this.email){
+      return "Sorry this email is taken.";
+    } else {
+      return -1;
+    }
+  }
+}
+
+function displayFunction(){
+  var arrLength = myAddresses.length;
+  for(var i = 0; i < arrLength; i ++){
+    var myCurrentAddress = myAddresses[i];
+    $("results").append("<p>" + "First Name: " + myCurrentAddress.first + "\n" + "Last Name: " + myCurrentAddress.last + "\n" + "Birthday: " + myCurrentAddress.birth + "\n" + "Address: " + myCurrentAddress.address + "</p>");
+  }
+}
+
+$(document).ready(function(){
+  $("#contact").click(function(){
+    toggleThePage();
+    displayFunction();
+  });
+  $("form#addressForm").submit(function(event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var birthday = $("#birthday").val();
+    var address = $("#address").val();
+    var phone = $("#phone").val();
+    var email = $("#email").val();
+    var CallThis = new address(returnName((name),0),returnName((name),1), birthday, address, phone,email)
+    checkFunction(object);
+    myAddresses.push(callThis);
+  });
+});
